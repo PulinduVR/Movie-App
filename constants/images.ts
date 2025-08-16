6c6076cf1498536df60e2605c4ac00e5bfd0bed8
PulinduVR <pulinduv@gmail.com>
2025-08-16 11:44:30 +0530

    Restore

---
+++ constants/images.ts
@@ -0,0 +1,17 @@
+import bg from "@/assets/images/bg.png";
+import highlight from "@/assets/images/highlight.png";
+import home from "@/assets/images/home.png";
+import rankingGradient from "@/assets/images/rankingGradient.png";
+import save from "@/assets/images/save.png";
+import search from "@/assets/images/search.png";
+import user from "@/assets/images/user.png";
+
+export const images = {
+  bg,
+  highlight,
+  rankingGradient,
+  home,
+  search,
+  save,
+  user
+};
