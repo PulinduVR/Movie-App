5130d4ddae18c6c3283d546556ca8f1ced3cc250
PulinduVR <pulinduv@gmail.com>
2025-08-16 19:04:48 +0530

    feat(home):movie rendering completed

---
+++ services/api.ts
@@ -0,0 +1,40 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+
+export const fetchPopularMovies = async ({query}:{query:string}) => {
+    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers
+    });
+
+    if(!response.ok) {
+        throw new Error(`Error fetching movies: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data.results;
+}
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTI4OTNlYWQ1YzMxZWRjM2ViZTk5MThhZmYwNTg2ZCIsIm5iZiI6MTc1NTMzNTYzNC4yNDk5OTk4LCJzdWIiOiI2OGEwNGJkMjM1NzE1MTU1ZjcwYmY4YmQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.X-fa3Ld-J1302chQTLyurAFmH0anG6_R4Jbhg0lPhDQ'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
